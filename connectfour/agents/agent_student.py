d5202717dd84bdfb5ff24886db6abda167d238e0
Adrn <adrianlimsongen@gmail.com>
2019-05-02 22:38:24 +1000

    added more evaluation

--- connectfour/agents/agent_student.py
+++ connectfour/agents/agent_student.py
@@ -32,27 +32,60 @@ class StudentAgent(RandomAgent):
     def dfMiniMax(self, board, depth, alpha, beta):
         # Goal return column with maximized scores of all possible next states
 
-        if depth == self.MaxDepth:
+        if depth >= self.MaxDepth:
             return self.evaluateBoardState(board)
 
         valid_moves = board.valid_moves()
+
+        result = 0
+        alpha = alpha
+        beta = beta
 
         for move in valid_moves:
             if depth % 2 == 1:
                 next_state = board.next_state(self.id % 2 + 1, move[1])
                 value = math.inf
+                print("1:" + str(value))
                 value = min(value, self.dfMiniMax(next_state, depth + 1, alpha, beta))
+                print("2:" + str(value))
                 beta = min(beta, value)
                 if alpha >= beta:
                     break
             else:
                 next_state = board.next_state(self.id, move[1])
                 value = -math.inf
+                print("3:" + str(value))
                 value = max(value, self.dfMiniMax(next_state, depth + 1, alpha, beta))
+                print("4:" + str(value))
                 alpha = max(alpha, value)
                 if alpha >= beta:
                     break
-            return value
+
+            result = value
+            return result
+        return result
+
+        # #==================
+        # vals = []
+        # moves = []
+        # for move in valid_moves:
+        #     if depth % 2 == 1:
+        #         next_state = board.next_state(self.id % 2 + 1, move[1])
+        #     else:
+        #         next_state = board.next_state(self.id, move[1])
+        #
+        #     moves.append( move )
+        #     vals.append( self.dfMiniMax(next_state, depth + 1) )
+        #
+        # # if move == 0:
+        # #     return 0;
+        #
+        # if depth % 2 == 1:
+        #     bestVal = min(vals)
+        # else:
+        #     bestVal = max(vals)
+        #
+        # return bestVal
 
     def evaluateBoardState(self, board):
         """
@@ -95,44 +128,44 @@ class StudentAgent(RandomAgent):
             row_array = board.board[r]
 
             if row_array[3] == (self.id):
-                score += 3
+                score += 30
             if row_array[2] == (self.id):
-                score += 2
+                score += 20
             if row_array[4] == (self.id):
-                score += 2
+                score += 20
             if row_array[1] == (self.id):
-                score += 1
+                score += 10
             if row_array[5] == (self.id):
-                score += 1
+                score += 10
             if row_array[3] == (self.id%2 +1):
-                score -= 3
+                score -= 30
             if row_array[2] == (self.id%2 +1):
-                score -= 2
+                score -= 20
             if row_array[4] == (self.id%2 +1):
-                score -= 2
+                score -= 20
             if row_array[1] == (self.id%2 +1):
-                score -= 1
+                score -= 10
             if row_array[5] == (self.id%2 +1):
-                score -= 2
+                score -= 20
 
         #HORIZONTAL CHECKING
         for r in range(board.height-1, -1, -1):
-            row_array = board.board[r]
+            row_array = [board.board[r][c] for c in range(board.width)]
             for c in range(board.width-3):
                 window = row_array[c:c+4]
 
                 if window.count(self.id)==4:
-                    score += 100
+                    score += 999
                 if window.count(self.id) == 3 and window.count(0) == 1:
-                    score += 20
+                    score += (100*self.count_in_a_row(window, self.id, 3))
                 if window.count(self.id) == 2 and window.count(0) == 2:
-                    score += 5
+                    score += (50*self.count_in_a_row(window, self.id, 2))
                 if window.count(self.id%2 + 1)==4:
-                    score -= 200
+                    score -= 999
                 if window.count(self.id%2 + 1) == 3 and window.count(0) == 1:
-                    score -= 80
+                    score -= (250*self.count_in_a_row(window, (self.id%2 +1), 3))
                 if window.count(self.id%2 + 1) == 2 and window.count(0) == 2:
-                    score -= 15
+                    score -= (85*self.count_in_a_row(window, (self.id%2 +1), 2))
 
         #VERTICAL CHECKING
         for c in range(board.width):
@@ -141,17 +174,17 @@ class StudentAgent(RandomAgent):
                 window = col_array[r:r+4]
 
                 if window.count(self.id)==4:
-                    score += 100
+                    score += 999
                 if window.count(self.id) == 3 and window.count(0) == 1:
-                    score += 20
+                    score += (130*self.count_in_a_row(window, self.id, 3))
                 if window.count(self.id) == 2 and window.count(0) == 2:
-                    score += 5
+                    score += (50*self.count_in_a_row(window, self.id, 2))
                 if window.count(self.id%2 + 1)==4:
-                    score -= 200
+                    score -= 999
                 if window.count(self.id%2 + 1) == 3 and window.count(0) == 1:
-                    score -= 80
+                    score -= (250*self.count_in_a_row(window, (self.id%2 +1), 3))
                 if window.count(self.id%2 + 1) == 2 and window.count(0) == 2:
-                    score -= 15
+                    score -= (85*self.count_in_a_row(window, (self.id%2 +1), 2))
 
 
         #DIAGONAL CHECKING (POSITIVE)
@@ -160,17 +193,17 @@ class StudentAgent(RandomAgent):
                 window = [board.board[r+i][c+i] for i in range(4)]
 
                 if window.count(self.id)==4:
-                    score += 100
+                    score += 999
                 if window.count(self.id) == 3 and window.count(0) == 1:
-                    score += 20
+                    score += (160*self.count_in_a_row(window, self.id, 3))
                 if window.count(self.id) == 2 and window.count(0) == 2:
-                    score += 5
+                    score += (50*self.count_in_a_row(window, self.id, 2))
                 if window.count(self.id%2 + 1)==4:
-                    score -= 200
+                    score -= 999
                 if window.count(self.id%2 + 1) == 3 and window.count(0) == 1:
-                    score -= 100
+                    score -= (250*self.count_in_a_row(window, (self.id%2 +1), 3))
                 if window.count(self.id%2 + 1) == 2 and window.count(0) == 2:
-                    score -= 15
+                    score -= (85*self.count_in_a_row(window, (self.id%2 +1), 2))
 
         #DIAGONAL CHECKING (NEGATIVE)
         for r in range(board.height-3):
@@ -178,17 +211,32 @@ class StudentAgent(RandomAgent):
                 window = [board.board[r+3-i][c+i] for i in range(4)]
 
                 if window.count(self.id)==4:
-                    score += 100
+                    score += 999
                 if window.count(self.id) == 3 and window.count(0) == 1:
-                    score += 20
+                    score += (160*self.count_in_a_row(window, self.id, 3))
                 if window.count(self.id) == 2 and window.count(0) == 2:
-                    score += 5
+                    score += (50*self.count_in_a_row(window, self.id, 2))
                 if window.count(self.id%2 + 1)==4:
-                    score -= 200
+                    score -= 999
                 if window.count(self.id%2 + 1) == 3 and window.count(0) == 1:
-                    score -= 100
+                    score -= (250*self.count_in_a_row(window, (self.id%2 +1), 3))
                 if window.count(self.id%2 + 1) == 2 and window.count(0) == 2:
-                    score -= 15
+                    score -= (85*self.count_in_a_row(window, (self.id%2 +1), 2))
 
-        print(score)
+        print("5:" + str(score))
         return score
+
+    #COUNTING HOW MANY IN A ROW
+    def count_in_a_row(self, list, id, num):
+        a = 0
+        b = 0
+        count = 0
+        for x in list:
+            if x == id and count<=num:
+                a += 1
+                count += 1
+            if x != id:
+                b = max(a,b)
+                a = 0
+
+        return max(a,b)
